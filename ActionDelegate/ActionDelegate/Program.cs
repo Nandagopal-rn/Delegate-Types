9aa524bbd8f79ccad0951f1cc4530f2352a3478b
Nandagopal-rn <119663637+Nandagopal-rn@users.noreply.github.com>
2022-12-06 15:50:17 +0530

    Add files via upload

---
+++ ActionDelegate/ActionDelegate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ActionDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Calculator using Action Delegate\n");
+
+            var ExecAddAction = new Action<int, int>(AddNumber);
+            PerformOperation(ExecAddAction);
+
+            var ExecDiffAction = new Action<int, int>(SubstractNumber);
+            PerformOperation(ExecDiffAction);
+
+            var ExecMultiplyAction = new Action<int, int>(MultiplyNumber);
+            PerformOperation(ExecMultiplyAction);
+
+            var ExecDivideAction = new Action<int, int>(DivisionNumber);
+            PerformOperation(ExecDivideAction);
+        }
+
+        static void PerformOperation(Action<int,int> Operations)
+        {
+            Operations.Invoke(50,20);
+        }
+
+        static void AddNumber(int FirstNumber,int SecondNumber)
+        {
+            Console.WriteLine($"Sum is {FirstNumber+SecondNumber}");
+        }
+        static void SubstractNumber(int FirstNumber, int SecondNumber)
+        {
+            Console.WriteLine($"Difference is {FirstNumber - SecondNumber}");
+        }
+        static void MultiplyNumber(int FirstNumber, int SecondNumber)
+        {
+            Console.WriteLine($"Product is {FirstNumber * SecondNumber}");
+        }
+        static void DivisionNumber(int FirstNumber, int SecondNumber)
+        {
+            Console.WriteLine($"Quotient is {FirstNumber / SecondNumber}");
+        }
+    }
+}
