533ee32e13fdeb6f9922705cdc4c53ef018da515
Nandagopal-rn <119663637+Nandagopal-rn@users.noreply.github.com>
2022-12-06 16:26:22 +0530

    Add files via upload

---
+++ PredicateDelegate/PredicateDelegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PredicateDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking Vowels using Predicate Delegate");
+            
+            var CheckVowel = new Predicate<char>(IsVowel);
+            DisplayLetter(CheckVowel);
+        }
+
+        static void DisplayLetter(Predicate<char> VowelDisplay)
+        {
+            Console.WriteLine(VowelDisplay.Invoke('a'));
+            Console.WriteLine(VowelDisplay.Invoke('A'));
+            Console.WriteLine(VowelDisplay.Invoke('E'));
+            Console.WriteLine(VowelDisplay.Invoke('s'));
+            Console.WriteLine(VowelDisplay.Invoke('X'));
+            Console.WriteLine(VowelDisplay.Invoke('i'));
+            Console.WriteLine(VowelDisplay.Invoke('k'));
+        }
+
+        static bool IsVowel(char letters)
+        {
+            bool IsVowelLetter = false;
+            switch (char.ToLower(letters))
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    IsVowelLetter = true;
+                    break;
+                default:
+                    break;
+            }
+            if (IsVowelLetter)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
